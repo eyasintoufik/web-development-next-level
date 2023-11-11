e064a98d3f337e0eb5b886a81e8fee310791e5f9
eyasintoufik <eyasinbhui@gmail.com>
2023-11-12 05:52:00 +0600

    first commit

---
+++ module1/src/index.ts
@@ -0,0 +1,9 @@
+const course = "Welcome to next web development course "; 
+console.log(course); 
+
+/*
+1.tsc ./module1/src/index.js
+configuaration file : 
+            tsc --init  //rootdir and outDir change 
+            then tsc 
+*/
