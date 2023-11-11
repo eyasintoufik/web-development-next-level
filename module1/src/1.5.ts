58687c5d47688fcadddda84b9b5cd174459bcb8e
eyasintoufik <eyasinbhui@gmail.com>
2023-11-12 05:59:56 +0600

    object

---
+++ module1/src/1.5.ts
@@ -0,0 +1,20 @@
+//reference type ====> 
+
+// const user = {
+//     firstName : 'Mezbaul ', 
+//     middleName : 'Abedin ', 
+//     lastName : 'Persian'
+// }
+
+const user:  {
+    company : 'Programming Hero' // -----type => literal types
+    firstName : string;  
+    middleName?: string; //optional type 
+    lastName : string;  
+    isMarried : boolean ; 
+} = {
+    company: 'Programming Hero', 
+    firstName: 'jhankar', 
+    lastName : 'Mahbub', 
+    isMarried : true,
+}; 
